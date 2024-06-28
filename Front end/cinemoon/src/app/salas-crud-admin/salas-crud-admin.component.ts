3dc6ae00a4d54ba962696cc2804348248c4a445b
drippy-uwu <brayanpichiule_@hotmail.com>
2024-06-28 01:45:27 -0500

    Cambios Brayan
    Back funciones y salas

---
+++ Front end/cinemoon/src/app/salas-crud-admin/salas-crud-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salas-crud-admin',
+  templateUrl: './salas-crud-admin.component.html',
+  styleUrl: './salas-crud-admin.component.css'
+})
+export class SalasCrudAdminComponent {
+
+}
